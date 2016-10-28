84cd5c9ece293bcc0fa85487654c7041b3bd65aa
mallikarjunveepuru <malliveepuru@gmail.com>
2016-10-28 21:24:48 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,11 +8,11 @@ rm -rf scala-2.10.5.deb
 wget www.scala-lang.org/files/archive/scala-2.10.5.deb
 dpkg -i scala-2.10.5.deb
 apt-get -f install -y
+apt-get install scala -y
 rm -rf /etc/apt/sources.list.d/sbt.list
 echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 apt-get update -y
-apt-get install unzip -y
 apt-get install sbt -y
 rm -rf kafka_2.10-0.9.0.1*
 apt-get install curl -y
@@ -21,11 +21,8 @@ sleep 15
 unzip kafka_2.10-0.9.0.1.zip
 ip="$(curl icanhazip.com)"
 sed -i "s/localhost/${ip}/g" ~/kafka_2.10-0.9.0.1/config/producer.properties
-sed -i "s/localhost/${ip}/g" ~/kafka_2.10-0.9.0.1/config/server.properties
-sed -i "s/2181/30000/g" ~/kafka_2.10-0.9.0.1/config/server.properties
-sed -i "s/127.0.0.1/${ip}/g" ~/kafka_2.10-0.9.0.1/config/consumer.properties
-sed -i "s/2181/30000/g" ~/kafka_2.10-0.9.0.1/config/consumer.properties 
-sed -i "s/2181/30000/g" ~/kafka_2.10-0.9.0.1/config/zookeeper.properties
 ~/kafka_2.10-0.9.0.1/bin/zookeeper-server-start.sh kafka_2.10-0.9.0.1/config/zookeeper.properties&
+
+
 #kafka_2.10-0.9.0.1/bin/kafka-server-start.sh kafka_2.10-0.9.0.1/config/server.properties&
 #kafka_2.10-0.9.0.1/bin/kafka-console-producer.sh --topic test --broker-list localhost:9092
